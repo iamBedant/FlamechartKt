d3ef080b5a2b613f91d09d62f286468711a91167
iamBedant <bebi2527@gmail.com>
2024-07-15 20:30:48 +0530

    Adds function to traverse trace tree

--- composeApp/src/commonMain/kotlin/processor/processor.kt
+++ composeApp/src/commonMain/kotlin/processor/processor.kt
@@ -2,5 +2,21 @@ package processor
 
 
 fun traverseLevelsForTraceTree(tree: TraceTree): HashMap<Int, List<TraceNode>> {
-    TODO()
+    val map = hashMapOf<Int, List<TraceNode>>()
+    var currentLevel = 0
+    val currentNode = mutableListOf<TraceTree>()
+    currentNode.add(tree)
+
+    while (currentNode.isEmpty().not()) {
+        val nextLevelChildren = mutableListOf<TraceTree>()
+
+        for (node in currentNode) {
+            nextLevelChildren.addAll(node.children)
+        }
+        map[currentLevel] = currentNode.map { it.data }
+        currentNode.clear()
+        currentNode.addAll(nextLevelChildren)
+        currentLevel++
+    }
+    return map
 }
