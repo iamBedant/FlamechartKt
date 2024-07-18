770615bf1e0ff3c48443486ebe1f85a5671765cc
iamBedant <bebi2527@gmail.com>
2024-07-18 22:44:12 +0530

    Adds a bidirectional scroll component

--- composeApp/src/commonMain/kotlin/ui/DetailScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/DetailScreen.kt
@@ -1,8 +1,11 @@
 package ui
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,15 +19,25 @@ fun DetailScreen(component: HashMap<Int, List<TraceNode>>, goBack: () -> Unit) {
         Text("GoBack", modifier = Modifier.clickable { goBack.invoke() })
         Spacer(Modifier.height(60.dp))
 
-        LazyColumn() {
-            for ((key, value) in component) {
-                item {
+        val stateVertical = rememberScrollState(0)
+        val stateHorizontal = rememberScrollState(0)
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(stateVertical)
+                .padding(end = 12.dp, bottom = 12.dp)
+                .horizontalScroll(stateHorizontal)
+        ){
+            Column {
+                for ((key, value) in component) {
                     Row {
                         value.forEach {
                             Text(it.functionName)
                             Spacer(Modifier.width(50.dp))
                         }
                     }
+                    Spacer(Modifier.height(10.dp))
                 }
             }
         }
